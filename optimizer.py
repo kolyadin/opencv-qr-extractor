be25aba37a55db769a9b0f62dfa4198a5d002366
kolyadin <aleksey.kolyadin@isobar.ru>
2019-03-28 00:52:39 +0300

    testing

---
+++ optimizer.py
@@ -0,0 +1,28 @@
+import cv2
+import extractor
+
+
+def zoomer(image):
+    decoded_data = None
+
+    for x in range(1, 10):
+        smaller = cv2.resize(image, (0, 0), fx=x / 10, fy=x / 10)
+        extracted = extractor.decode(smaller)
+
+        if extracted is not None:
+            decoded_data = extracted
+            break
+
+    return decoded_data
+
+
+def gray(image):
+    decoded_data = None
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # extracted = extractor.decod(gray)
+
+    if extracted is not None:
+        decoded_data = extracted
+
+    return decoded_data
