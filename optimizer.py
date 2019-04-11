8723f0f7bd43701f9ebe870c5dafc054a5729d64
kolyadin <aleksey.kolyadin@isobar.ru>
2019-04-12 01:51:43 +0300

    improve zoomer filter

--- optimizer.py
+++ optimizer.py
@@ -5,7 +5,7 @@ import extractor
 def zoomer(image):
     decoded_data = None
 
-    for x in range(1, 100):
+    for x in range(1, 10):
         smaller = cv2.resize(image, (0, 0), fx=x / 10, fy=x / 10)
         smaller = cv2.cvtColor(smaller, cv2.COLOR_BGR2GRAY)
 
@@ -13,7 +13,21 @@ def zoomer(image):
 
         if extracted is not None:
             decoded_data = extracted
+            print("Zoomer match reduce factor", x)
             break
+
+    if decoded_data is None:
+
+        for x in range(1, 3):
+            smaller = cv2.resize(image, (0, 0), fx=x * 1, fy=x * 1)
+            smaller = cv2.cvtColor(smaller, cv2.COLOR_BGR2GRAY)
+
+            extracted = extractor.decode(smaller)
+
+            if extracted is not None:
+                decoded_data = extracted
+                print("Zoomer match enlarge factor", x)
+                break
 
     return decoded_data
 
